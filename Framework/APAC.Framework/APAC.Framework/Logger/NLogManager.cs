96674d5704784dbc965110d8023a9279f1ec9b7c
Aman KUMAR <amkumar@global.lpl.top>
2018-08-24 06:19:52 +0000

    modified:   Framework/APAC.Framework/APAC.Framework.v12.suo 	modified:   Framework/APAC.Framework/APAC.Framework/Logger/NLogManager.cs 	modified:   Framework/APAC.Framework/APAC.Framework/bin/Debug/APAC.Framework.dll 	modified:   Framework/APAC.Framework/APAC.Framework/bin/Debug/APAC.Framework.pdb 	modified:   Framework/APAC.Framework/APAC.Framework/obj/Debug/APAC.Framework.ClrTypes.dll 	modified:   Framework/APAC.Framework/APAC.Framework/obj/Debug/APAC.Framework.btproj.FileListAbsolute.txt 	modified:   Framework/APAC.Framework/APAC.Framework/obj/Debug/APAC.Framework.dll 	modified:   Framework/APAC.Framework/APAC.Framework/obj/Debug/APAC.Framework.pdb 	modified:   Framework/APAC.Framework/APAC.Framework/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache

--- Framework/APAC.Framework/APAC.Framework/Logger/NLogManager.cs
+++ Framework/APAC.Framework/APAC.Framework/Logger/NLogManager.cs
@@ -11,7 +11,18 @@ namespace APAC.Framework.HelperLibrary
     [Serializable()]
     public static class NLogManager
     {
+        #region Public Constants
+        public const string LogDebug = "DEBUG";
+
+
+        #endregion
+
         private static readonly Logger logman = LogManager.GetCurrentClassLogger();
+
+        private const string ServiceNotSet = "Service Not Specified";
+        private const string SearchKeyNotSet = "Search Key Not Specified";
+
+        #region Old Code
         public static void Log(LogType level, string message, Exception exception)
 
         {
@@ -54,11 +65,69 @@ namespace APAC.Framework.HelperLibrary
                     break;
             }
         }
+        #endregion
 
         public static void ScopeIn(string Scopename, string message)
         {
 
         }
+
+        public static void Debug(object message, string componentName, string projectName, Guid traceToken)
+        {
+            WriteNlogNetDetails(logman, message == null ? string.Empty : message.ToString(), componentName, ServiceNotSet, projectName, traceToken, SearchKeyNotSet, LogDebug);
+        }
+
+        private static void WriteNlogNetDetails(Logger logger, string logtowrite, string componentName, string serviceName, string projectName, Guid traceToken,string searchKey, string logLevel, Exception exToLog = null)
+        {
+            try
+            {
+                LogEventInfo currentLogEvent = null;
+                switch (logLevel)
+                {
+                    case LogDebug:
+                        currentLogEvent = new LogEventInfo(LogLevel.Debug, logger.Name, "");
+                        currentLogEvent.Message = logtowrite;
+                        currentLogEvent = CreateNLogEvent(currentLogEvent, logtowrite, componentName, serviceName, projectName, traceToken, searchKey, logLevel);
+                        logger.Log(currentLogEvent);
+                        break;
+                    default:
+                        currentLogEvent = new LogEventInfo(LogLevel.Info, logger.Name, "");
+
+                        if (exToLog == null)
+                        {
+                            currentLogEvent.Message = logtowrite;
+                        }
+                        else
+                        {
+                            currentLogEvent.Level = LogLevel.Error;
+                            currentLogEvent.Message = !string.IsNullOrWhiteSpace(logtowrite) ? logtowrite : (exToLog.InnerException).ToString();
+                            currentLogEvent.Exception = exToLog;
+                        }
+                        break;
+                }
+               
+
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+        private static LogEventInfo CreateNLogEvent(LogEventInfo currentLogEvent, string logtowrite, string componentName, string serviceName, string projectName, Guid traceToken, string searchKey, string logLevel)
+        {
+            currentLogEvent.Properties["loggedAt"] = DateTime.UtcNow.ToString("yyyy.MM.dd HH:mm:ss:fff");
+            currentLogEvent.Properties["serverName"] = Environment.MachineName;
+            currentLogEvent.Properties["componentName"] = componentName;
+            currentLogEvent.Properties["serviceName"] = serviceName;
+            currentLogEvent.Properties["projectName"] = projectName;
+            currentLogEvent.Properties["traceToken"] = traceToken;
+            currentLogEvent.Properties["searchKey"] = searchKey;
+            currentLogEvent.Properties["logLevel"] = logLevel;
+            return currentLogEvent;
+        }
+
     }
 
     [Serializable()]
